525903ae25d968ae32a3897e5333f52a86fbffcd
sinbad-thethird <140252505+sinbad-thethird@users.noreply.github.com>
2023-12-21 02:16:20 +0700

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ from io import StringIO
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://fooders.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
